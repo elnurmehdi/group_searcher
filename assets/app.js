aa8f2d7f57bd37376ed6c1ae790d5503d6908747
elnurmehdi <elnurkm@code.edu.az>
2022-09-17 18:29:55 +0400

    initial commit

---
+++ assets/app.js
@@ -0,0 +1,55 @@
+
+
+function Show()
+{
+    var x = document.getElementById('group').value  
+    var grNum = x.slice(-3, -2) 
+
+    document.getElementById('groupNumber').innerHTML=x
+    
+   
+ 
+
+    if(grNum==1){
+        document.getElementById('shifts').style.textContent="asdfqibirjm"
+        document.getElementById('groupNumber').style.color="yellow"
+        alert("Morning shifts")
+        
+    }
+
+    else if(grNum==2){
+        document.getElementById('groupNumber').style.color="red"
+        alert("Afternoon shifts")
+    }
+
+    else if(grNum==3){
+        alert("Night shifts")
+        document.getElementById('groupNumber').style.color="black"
+    
+    } 
+
+  
+
+    
+}
+
+function Mode() {
+ 
+ if ( document.body.style.backgroundColor=="white") {
+    
+     document.body.style.backgroundColor="black"
+     document.getElementById("btn").textContent="Light mode"
+    
+
+    }
+    
+ else 
+ {
+    document.body.style.backgroundColor="white"
+    document.getElementById("btn").textContent="Dark mode"
+    
+ }
+    
+}
+
+
